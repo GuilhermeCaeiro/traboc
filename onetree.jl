443b6b8104beadf9448fca71761532a9b7132114
Guilherme Caeiro de Mattos <guilhermecaeirodemattos@gmail.com>
2022-09-15 20:12:42 -0300

    Fixing variable undefined

--- onetree.jl
+++ onetree.jl
@@ -33,8 +33,8 @@ end
 # Returns
 # mst -> the minimum spanning tree turned into a 1-tree.
 function one_tree_graph(cost_matrix, num_nodes)
-    graph = partial_graph(n, 1)
-    mst, c = minimum_spanning_tree(graph, cost_matrix, n)
+    graph = partial_graph(num_nodes, 1)
+    mst, c = minimum_spanning_tree(graph, cost_matrix, num_nodes)
     # Adding the two cheapest edges from node 1
     sorted_nodes = sortperm(cost_matrix[1, 2:end]) # must ignore the first column
     add_edge!(mst, 1, sorted_nodes[1] + 1) # + 1 to compensate for the indice of the first column that is ignored in the previous line 
